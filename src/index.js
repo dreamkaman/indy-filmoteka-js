7838013f1525036a91e18080314bce276ada7b01
Dreamkaman <riabushko@gmail.com>
2023-08-23 23:33:14 +0300

    Added corrections to the structure

--- src/index.js
+++ src/index.js
@@ -2,9 +2,5 @@
 import './index.html'; //for 'html-loader'
 import './partials/header.html';
 import './partials/footer.html';
-import './partials/hero.html';
-import './partials/section-about.html';
-import './partials/section-contact-us.html';
-import './partials/section-our-projects.html';
-import './partials/section-our-tasks.html';
+import './partials/section-films.html';
 import './index.scss'; //for 'mini-css-extract-plugin'
