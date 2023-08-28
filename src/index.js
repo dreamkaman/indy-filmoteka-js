449af8336d891a34721541368aeaa755f49b65d4
Dreamkaman <riabushko@gmail.com>
2023-08-28 19:11:05 +0300

    Changed webpack settings. Added new page, activeMenuScript.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // import index files block
 import './index.html'; //for 'html-loader'
+import './library.html'; //for 'html-loader'
 import './partials/header.html';
 import './partials/footer.html';
 import './partials/section-films.html';
 import './index.scss'; //for 'mini-css-extract-plugin'
+
+import './scripts/activeMenuItem';
---
+++ src/library.js
@@ -0,0 +1,4 @@
+import './library.html'; //for 'html-loader'
+import './index.scss'; //for 'mini-css-extract-plugin'
+
+import './scripts/activeMenuItem';
