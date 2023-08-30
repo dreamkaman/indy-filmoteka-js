32309a33123783435d5f0c06168318e4be3f52fb
Dreamkaman <riabushko@gmail.com>
2023-08-31 01:14:20 +0300

    Added corrections to the paginations styles

--- src/scripts/pagination.js
+++ src/scripts/pagination.js
@@ -12,37 +12,6 @@ export const paginationOptions = {
       '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
       '<span class="tui-ico-{{type}}">{{type}}</span>' +
       '</a>',
-
-    // moveButton: (type) => {
-    //   let template = '<div></div>';
-    //   if (type === 'first') {
-    //     template =
-    //       '<a href="#" class="tui-page-btn tui-first custom-class-first">' +
-    //       '<span class="tui-ico-first">first</span>' +
-    //       '</a>';
-    //   }
-    //   if (type === 'last') {
-    //     template =
-    //       '<a href="#" class="tui-page-btn tui-last custom-class-last">' +
-    //       '<span class="tui-ico-last">last</span>' +
-    //       '</a>';
-    //   }
-
-    //   if (type === 'prev') {
-    //     template =
-    //       '<a href="#" class="tui-page-btn tui-prev custom-class-prev">' +
-    //       '<span class="tui-ico-prev">prev</span>' +
-    //       '</a>';
-    //   }
-    //   if (type === 'next') {
-    //     template =
-    //       '<a href="#" class="tui-page-btn tui-next custom-class-next">' +
-    //       '<span class="tui-ico-next">next</span>' +
-    //       '</a>';
-    //   }
-
-    //   return template;
-    // },
     disabledMoveButton:
       '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
       '<span class="tui-ico-{{type}}">{{type}}</span>' +
