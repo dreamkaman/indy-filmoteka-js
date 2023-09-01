e09495acd885a0ef380725002cb879f8d2d2b373
Dreamkaman <riabushko@gmail.com>
2023-09-01 23:42:50 +0300

    Added corrections to the pagination logic

--- src/API/api.js
+++ src/API/api.js
@@ -1,51 +1,53 @@
 const API_KEY = process.env.TMDB_API_READ_ACCESS_TOKEN;
 
 const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: `Bearer ${API_KEY}`,
-  },
+	method: 'GET',
+	headers: {
+		accept: 'application/json',
+		Authorization: `Bearer ${API_KEY}`,
+	},
 };
 
 export const getPopularMovies = async (page = 1) => {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/trending/movie/week?language=en-US&page=${page}`,
-    options,
-  );
-  const data = await response.json();
+	const response = await fetch(
+		`https://api.themoviedb.org/3/trending/movie/week?language=en-US&page=${page}`,
+		options,
+	);
+	const data = await response.json();
 
-  return data;
+	console.log(data);
+
+	return data;
 };
 
 export const getMoviesGenres = async () => {
-  const response = await fetch(
-    'https://api.themoviedb.org/3/genre/movie/list?language=en',
-    options,
-  );
+	const response = await fetch(
+		'https://api.themoviedb.org/3/genre/movie/list?language=en',
+		options,
+	);
 
-  const data = await response.json();
+	const data = await response.json();
 
-  return data.genres;
+	return data.genres;
 };
 
 export const getTVGenres = async () => {
-  const response = await fetch('https://api.themoviedb.org/3/genre/tv/list?language=en', options);
+	const response = await fetch('https://api.themoviedb.org/3/genre/tv/list?language=en', options);
 
-  const data = await response.json();
+	const data = await response.json();
 
-  return data.genres;
+	return data.genres;
 };
 
 export const getMovieByName = async (movieName, page = 1) => {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/search/movie?query=${movieName}&include_adult=false&language=en-US&page=${page}`,
-    options,
-  );
+	const response = await fetch(
+		`https://api.themoviedb.org/3/search/movie?query=${movieName}&include_adult=false&language=en-US&page=${page}`,
+		options,
+	);
 
-  const data = await response.json();
+	const data = await response.json();
 
-  return data;
+	return data;
 };
 
 const moviesGenres = await getMoviesGenres();
--- src/tools/transformMovies.js
+++ src/tools/transformMovies.js
@@ -4,9 +4,10 @@ import img from '../assets/noposter.jpg';
 
 export const transformPopularMovies = (popularMovies, allGenres) => {
 	const transformedPopularMovies = popularMovies.map((popularMovie) => {
-		const { genre_ids, title, release_date, poster_path } = popularMovie;
+		const { id, genre_ids, title, release_date, poster_path } = popularMovie;
 
 		const newMovie = {
+			id,
 			title: title.length < 40 ? title : title.slice(0, 37) + '...',
 			year: getYear(release_date) || 'Unknown release date',
 			posterPath: poster_path ? `https://www.themoviedb.org/t/p/w1280${poster_path}` : img,
