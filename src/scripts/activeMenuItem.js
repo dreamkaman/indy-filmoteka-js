4a27d2b16bbe7b296ddc26284b563b9cdcfce677
Dreamkaman <riabushko@gmail.com>
2023-09-09 23:23:19 +0300

    Added radio buttons

--- src/scripts/activeMenuItem.js
+++ src/scripts/activeMenuItem.js
@@ -1,5 +1,5 @@
-const HOME_PATH = '/';
-const LIBRARY_PATH = '/library';
+const HOME_PATH = 'index.html'; //'/' | '/index.html';
+const LIBRARY_PATH = '/library.html'; //'/library' | '/library.html';
 
 const menu = document.querySelectorAll('li.page-header__menu-item');
 const headerBtnWrapper = document.querySelector('div.header-buttons-wrapper');
