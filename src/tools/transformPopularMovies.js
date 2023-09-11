c7afaff6dafd0213b85b011989a0a138abe66412
Dreamkaman <riabushko@gmail.com>
2023-09-11 17:05:48 +0300

    Added corrections to the tools logic and handlebars templates

--- src/tools/transformPopularMovies.js
+++ src/tools/transformPopularMovies.js
@@ -5,13 +5,18 @@ import img from '../assets/noposter.jpg';
 export const transformPopularMovies = (popularMovies, allGenres) => {
 	const transformedPopularMovies = popularMovies.map((popularMovie) => {
 		const { id, genre_ids, title, release_date, poster_path } = popularMovie;
-
 		const newMovie = {
 			id,
+			titleFull: title,
 			title: title.length < 40 ? title : title.slice(0, 37) + '...',
 			year: getYear(release_date) || 'Unknown release date',
 			posterPath: poster_path ? `https://www.themoviedb.org/t/p/w1280${poster_path}` : img,
-			genres: genre_ids.length ? transformGenres(genre_ids, allGenres) : 'Unknown genres',
+			genres: genre_ids?.length
+				? transformGenres(genre_ids, allGenres).genresStr
+				: 'Unknown genres',
+			genresFull: genre_ids?.length
+				? transformGenres(genre_ids, allGenres).genresFull
+				: 'Unknown genres',
 		};
 
 		return newMovie;
