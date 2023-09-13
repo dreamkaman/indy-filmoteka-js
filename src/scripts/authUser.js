4daaf9de3b68f60209964fb7052abc825746b335
Dreamkaman <riabushko@gmail.com>
2023-09-14 00:01:53 +0300

    Next login form

--- src/scripts/authUser.js
+++ src/scripts/authUser.js
@@ -1,5 +1,11 @@
 import { showModal } from './modalWindow';
+import template from '../handlebars/loginRegistration.hbs';
 
 const svgUser = document.querySelector('.user-icon');
+const modalWindowForm = document.querySelector('form.modal-form');
+const loginRegisterMarkUp = template();
 
-svgUser.addEventListener('click', () => showModal('auth'));
+svgUser.addEventListener('click', () => {
+	showModal('auth');
+	modalWindowForm.innerHTML = loginRegisterMarkUp;
+});
